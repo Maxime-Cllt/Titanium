d790d0d29bd205f8e1f2dbf0fea2b55124770eac
mcolliat21 <mcolliat21@gmail.com>
2022-10-18 12:20:19 +0200

    Amélioration des commentaires des classes .cpp pour rendu Doxygen

--- Contact/QtContact.cpp
+++ Contact/QtContact.cpp
@@ -9,120 +9,188 @@
 #include "QtContact.h"
 #include "StdContact.h"
 
-//Constructeur principal de la classe QtContact
+
+/**
+ * @details Constructeur de la classe QtContact
+ * @param nom
+ * @param prenom
+ * @param entreprise
+ * @param mail
+ * @param telephone
+ * @param photo
+ * @param dateCreation
+ * @param lstInteraction
+ */
 QtContact::QtContact(const QString &nom, const QString &prenom, const QString &entreprise, const QString &mail,
                      const QString &telephone, const QString &photo, const uint64_t &dateCreation,
                      const ListInteraction &lstInteraction) : Nom(nom), Prenom(prenom), Entreprise(entreprise),
                                                               Mail(mail), Telephone(telephone), Photo(photo),
                                                               DateCreation(dateCreation),
-                                                              lstInteraction(new ListInteraction(lstInteraction)) {}
+                                                              lstInteraction(new ListInteraction(lstInteraction))
+{}
 
-//Getter de l'attribut Nom
+/**
+ *
+ * @return le Nom du contact
+ */
 const QString &QtContact::getNom() const
 {
     return Nom;
 }
 
-//Setter de l'attribut Nom
+/**
+ *
+ * @param Nom du contact
+ */
 void QtContact::setNom(const QString &nom)
 {
     Nom = nom;
 }
 
-//Getter de l'attribut Prenom
+/**
+ *
+ * @return Le Prenom du contact
+ */
 const QString &QtContact::getPrenom() const
 {
     return Prenom;
 }
 
-//Setter de l'attribut Prenom
+/**
+ *
+ * @param Prenom du contact
+ */
 void QtContact::setPrenom(const QString &prenom)
 {
     Prenom = prenom;
 }
 
-//Getter de l'attribut Entreprise
+/**
+ *
+ * @return L'Entreprise du contact
+ */
 const QString &QtContact::getEntreprise() const
 {
     return Entreprise;
 }
 
-//Setter de l'attribut Entreprise
+/**
+ *
+ * @param Entreprise du contact
+ */
 void QtContact::setEntreprise(const QString &entreprise)
 {
     Entreprise = entreprise;
 }
 
-//Getter de l'attribut Mail
+/**
+ *
+ * @return Le Mail du contact
+ */
 const QString &QtContact::getMail() const
 {
     return Mail;
 }
 
-//Setter de l'attribut Mail
+/**
+ *
+ * @param Mail du contact
+ */
 void QtContact::setMail(const QString &mail)
 {
     Mail = mail;
 }
 
-//Getter de l'attribut Telephone
+/**
+ *
+ * @return Le Telephone du contact
+ */
 const QString &QtContact::getTelephone() const
 {
     return Telephone;
 }
 
-//Setter de l'attribut Telephone
+/**
+ *
+ * @param Telephone du contact
+ */
 void QtContact::setTelephone(const QString &telephone)
 {
     Telephone = telephone;
 }
 
-//Getter de l'attribut Photo
+/**
+ *
+ * @return La Photo du contact
+ */
 const QString &QtContact::getPhoto() const
 {
     return Photo;
 }
 
-//Setter de l'attribut Photo
+/**
+ *
+ * @param Photo du contact
+ */
 void QtContact::setPhoto(const QString &photo)
 {
     Photo = photo;
 }
 
-//Constructeur par défaut de la classe QtContact
-QtContact::QtContact() {}
+/**
+ * @param Constructeur par defaut d'un Contact
+ */
+QtContact::QtContact()
+{}
 
-//Getter de l'attribut lstInteraction
+/**
+ *
+ * @return La liste d'interaction du contact lstInteraction
+ */
 const ListInteraction QtContact::getLstInteraction() const
 {
     return *lstInteraction;
 }
 
-//Setter de l'attribut lstInteraction
+/**
+ *
+ * @param lstInteraction du contact
+ */
 void QtContact::setLstInteraction(const ListInteraction &lstInteraction)
 {
     QtContact::lstInteraction = new ListInteraction(lstInteraction);
 }
 
-//Getter de l'attribut lstInteraction
+/**
+ *
+ * @return La liste d'interaction du contact lstInteraction
+ */
 ListInteraction *QtContact::getLstInteraction()
 {
     return lstInteraction;
 }
 
-//Getter de l'attribut DateCreation
-
+/**
+ *
+ * @return DateCreation de creation du contact
+ */
 uint64_t QtContact::getDateCreation() const
 {
     return DateCreation;
 }
 
-//Setter de l'attribut DateCreation
+/**
+ *
+ * @param DateCreation du contact
+ */
 void QtContact::setDateCreation(uint64_t dateCreation)
 {
     DateCreation = dateCreation;
 }
 
+/**
+ * @details Destructeur de la classe QtContact
+ */
 QtContact::~QtContact()
 {
     delete lstInteraction;
